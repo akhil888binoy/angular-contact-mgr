0b7e8b9abae52e1fef996566ac1667e355cf9b98
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2022-11-10 22:51:20 +0530

    adding view contact

--- src/app/components/view-contact/view-contact.component.ts
+++ src/app/components/view-contact/view-contact.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from " @angular/router";
+import { Params } from '@angular/router';
+import { IContact } from 'src/app/models/IContact';
+import { IGroup } from 'src/app/models/IGroup';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-view-contact',
@@ -6,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-contact.component.css']
 })
 export class ViewContactComponent implements OnInit {
+  public loading:boolean =false;
+  public contactId: string |null =null;
+  public contact: IContact={} as IContact;
+  public errorMessage : string | null= null;
+  public group : IGroup ={} as IGroup;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+  private contactService: ContactService) { }
 
   ngOnInit(): void {
-  }
+    this.activatedRoute.paramMap.subscribe(next: (param: Params)=>{
+      this.contactId= param.get('contactId');
+    });
+    if(this.contactId){
+      this.loading = true;
+        this.contactService.getContact(this.contactId).subscribe(next: (data: IContact)=>{
+            this.contact = data;
+            this.loading =false;
+            this.contactService.getGroup(data).subscribe(next:(data: IGroup)=>{
+              this.group = data;
 
+            })
+        }, error: (error)=>{
+          this.errorMessage=error;
+          this.loading =false;
+
+        })
+    }
+
+  }
+  public isNotEmpty(){
+    return Object.keys(this.contact).length >0;
+
+  }
 }
