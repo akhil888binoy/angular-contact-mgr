829f03f217f0586a5aa8fe29d5b78436865c56f7
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2022-11-11 16:12:49 +0530

    add contact

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-contact.component.css']
 })
 export class AddContactComponent implements OnInit {
+  public loading : boolean =false;
+  public conatc : IContact = {} as IContact;
+  public errorMessage : string |null =null;
+  public groups: IGroup[]= [] as IGroup[];
 
-  constructor() { }
+  constructor(private ContactService : ContactService) { }
 
   ngOnInit(): void {
+    this.contactService.getAllGroups().subscribe(next:(data:IGroup)=>{
+      this.groups =data;
+    }, error(error)=>{
+      this.errorMessage=error;
+      
+    })
   }
 
 }
